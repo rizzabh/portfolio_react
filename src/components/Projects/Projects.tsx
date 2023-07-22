17320e76986d16f1b18884f8a33fc49582bbf4ec
Rishabh Pandey <133371863+rizzabh@users.noreply.github.com>
2023-07-22 22:30:23 +0530

    Update Projects.tsx

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import {Swiper,SwiperSlide, useSwiper} from 'swiper/react'
 import "swiper/css"
 import './Projects.css'
-import data from '../../utils/slider.json'
+import data from '../../utils/slider.json';
+
+
 const Projects = () => {
   return (
     <div>
@@ -10,16 +12,43 @@ const Projects = () => {
         <div className="paddings innerWidth p-container">
             <span className="primaryText p-head">
                 My Projects
-            </span>
-        </div>
-        <Swiper>
+            </span><br/><br/>
+        
+        <Swiper spaceBetween={60} slidesPerView={3}>
+          <SlideButtons/>
             {
-                <img src="" alt="" />
+                data.map((card, i) => (
+                  <SwiperSlide key={i}>
+                    <div className="flexCenter flexColStart p-card">
+                      <img src={card.image} alt="Thumb" />
+                      <span className="p-cardtext"><br/>
+                        <span className="primaryText name">{card.name}</span><br/>
+                        <span className="secondaryText detail">{card.detail}</span><br/>
+                        <a href={card.link1}>
+                        <button className="button2">Code</button></a>
+                        
+                    </span>
+ 
+                    </div>
+                  </SwiperSlide>
+
+                ))
             }
         </Swiper>
+        </div>
       </section>
     </div>
   )
 }
 
-export default Projects
+export default Projects;
+
+const SlideButtons =()=>{
+  const swiper = useSwiper();
+  return (
+    <div className="swipe">
+      <button onClick={()=>swiper.slidePrev()}>&lt;</button>
+      <button onClick={()=>swiper.slideNext()}>&gt;</button>
+    </div>
+  )
+}
