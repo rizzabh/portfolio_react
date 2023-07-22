b0dd22e2eb76e933b1aab2ec215280e77e5383a5
Rishabh Pandey <133371863+rizzabh@users.noreply.github.com>
2023-07-22 22:29:26 +0530

    Update Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -20,7 +20,7 @@ const Hero = () => {
                 <span>I Talk About App Development, UI-UX, and Cinema</span>
             </div>
             <div className="cta1">
-            <a href = "/public/Rishabh_Pandey_Resume_FE.pdf" download='Rishabh Resume'>
+            <a href = "/public/Rishabh_Pandey_Resume_FE.pdf">
             <button className="button2">Resume</button></a>
 
         </div>
