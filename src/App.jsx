c75deb56fd26bad76fab0713f21d43ca5092dd09
rizzabh <pandeyrishabh966@gmail.com>
2023-07-30 21:16:04 +0530

    Ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
 
       <Hero />
       <Projects />
+      <Contact />
     </>
   );
 }
