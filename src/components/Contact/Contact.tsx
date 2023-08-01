185bfdaabbbe08126d23feedd0a0f241297beff7
rizzabh <pandeyrishabh966@gmail.com>
2023-08-01 21:56:21 +0530

    Contact form created

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Contact.css'
 import emailjs from '@emailjs/browser'
-import { useRef } from 'react';
+
 export default function Contact() {
     const form = useRef();
+    
     
 
   const sendEmail = (e) => {
     e.preventDefault();
+    
+    const currentForm = form.current;
+    // this prevents sending emails if there is no form.
+    // in case currentForm cannot possibly ever be null,
+    // you could alert the user or throw an Error, here
+    if (currentForm == null) return;
+    
 
-    emailjs.sendForm('service_56ecw0e', 'template_qwtt4xt', form.current, 'mZLwBfQRFceD3wXsI')
+    emailjs.sendForm('service_56ecw0e', 'template_qwtt4xt',currentForm, 'mZLwBfQRFceD3wXsI')
       .then((result) => {
-          console.log(result.text);
+          alert("Email Sent");
+          console.log(result.text)
       }, (error) => {
+
+        alert("Please Fill all of the details");
           console.log(error.text);
       });
     }
@@ -23,21 +34,23 @@ export default function Contact() {
         <span className="primaryText p-head" id='Contact'>
                 Contact Me
             </span><br/><br/>
-            <div className="contact-cont">
-                <img src="./martin-martz-npdGyujDvaU-unsplash 1.png" alt="" />
+            <div className="contact-cont ">
+                <div className="img-green"><img src="./martin-martz-npdGyujDvaU-unsplash 1.png" alt="" /></div>
                 <div className="form-layout paddings">
                 <span className="primaryText p-head paddings">
                 Contact Form
             </span><br/><br/>
-            <form ref={form} onSubmit={sendEmail}>
+            
+            <form ref={form} onSubmit={sendEmail} >
       <label className='Your'>Your Name</label>
       <input type="text" name="user_name" />
       <label className='Your'>Your Email</label>
       <input type="email" name="user_email" />
       <label className='Your'>Message</label>
       <textarea name="message" />
-      <input type="submit" value="Send" />
+      <input className='sub-button' type="submit" value="Send" />
     </form>
+   
             
                 </div>
             </div>
