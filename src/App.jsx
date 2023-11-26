78e5f0d41c5251b028e143cba41a86e8dc177513
rizzabh <pandeyrishabh966@gmail.com>
2023-11-26 19:29:14 +0530

    therapy

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Work from "./components/Work/Work";
-import Gallery from "./components/Gallery/Gallery";
 function App() {
   return (
     <>
       <Header />
       <Hero />
       <Projects />
-      <Gallery />
       <Work />
       <Contact />
     </>
